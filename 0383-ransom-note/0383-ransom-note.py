d41bb109c414988032ddad6f912672f647948153
Joel Stanly A <38435145+JoelStanly@users.noreply.github.com>
2025-07-05 06:51:15 +0530

    Time: 8 ms (92.01%), Space: 18.1 MB (34.1%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,12 @@
+from collections import Counter
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransomNote = Counter(ransomNote)
+        magazine = Counter(magazine)
+
+        for key,value in ransomNote.items():
+            if key not in magazine:
+                return False
+            if magazine[key] < value:
+                return False
+        return True
